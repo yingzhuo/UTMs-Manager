c5bde7fedfddf727b12ca0cd67fb44cea02ef844
应卓 <yingzhor@gmail.com>
2024-12-31 04:29:36 +0800

    2024-12-31 04:29:36

--- sync.sh
+++ sync.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-rsync -av -e 'ssh -p 22' /Users/yingzhuo/Dev/UTMs-Manager/SYNC_FOLDER/ yingzhuo@192.168.31.165:/home/yingzhuo/upload/
-rsync -av -e 'ssh -p 22' /Users/yingzhuo/Dev/UTMs-Manager/SYNC_FOLDER/ yingzhuo@192.168.31.166:/home/yingzhuo/upload/
-rsync -av -e 'ssh -p 22' /Users/yingzhuo/Dev/UTMs-Manager/SYNC_FOLDER/ yingzhuo@192.168.31.167:/home/yingzhuo/upload/
+# 参考:
+#  1  https://www.ruanyifeng.com/blog/2020/08/rsync.html
+#  2  https://www.jianshu.com/p/bd3ae9d8069c
+
+rsync -av -delete --exclude='.gitkeep' -e 'ssh -p 22' /Users/yingzhuo/Dev/UTMs-Manager/SYNC_FOLDER/ yingzhuo@192.168.31.165:/home/yingzhuo/upload/
+rsync -av -delete --exclude='.gitkeep' -e 'ssh -p 22' /Users/yingzhuo/Dev/UTMs-Manager/SYNC_FOLDER/ yingzhuo@192.168.31.166:/home/yingzhuo/upload/
+rsync -av -delete --exclude='.gitkeep' -e 'ssh -p 22' /Users/yingzhuo/Dev/UTMs-Manager/SYNC_FOLDER/ yingzhuo@192.168.31.167:/home/yingzhuo/upload/
